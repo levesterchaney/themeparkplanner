5c002281ec82b9d2be64e8fa455719dfe1188876
Levester Chaney <levester.chaney@gmail.com>
2026-04-08 01:05:10 -0700

    initial next.js app

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,8 @@
+// Export all API services from a central location
+export { healthService } from './health';
+
+// Future service exports will go here:
+// export { parksService } from './parks';
+// export { attractionsService } from './attractions';
+// export { itinerariesService } from './itineraries';
+// export { usersService } from './users';
